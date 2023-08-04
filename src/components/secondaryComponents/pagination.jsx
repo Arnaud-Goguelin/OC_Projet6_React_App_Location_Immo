96276bbebcb9a556d1787670cd37486680b3e23d
Arnaud Goguelin <arnaud.goguelin@gmail.com>
2023-08-04 23:33:58 +0200

    révision du code pour permettre l'affiche de la page au clic sur son n°

--- src/components/secondaryComponents/pagination.jsx
+++ src/components/secondaryComponents/pagination.jsx
@@ -1,16 +1,41 @@
 import '../../assets/style/secondarycomponentsStyle/pagination.scss'
 
-function Pagination({ currentPage, maxPage, nextPage, previousPage }) {
+function Pagination({ currentPage, maxPage, nextPage, previousPage, selectPage }) {
 
-    let pages=[1,2,3,4]
+    let pages=[]
+
+    function completePages() {
+        for (let p = 1; p <= maxPage; p++) {
+            pages.push(p)
+        }
+    }
+
+    completePages()
 
     return (
         <ul className="pagination__container">
-            <button onClick={previousPage} className="pagination__Button navigatation previous" aria-label='page précédente'></button>
+            <button 
+                onClick={previousPage} 
+                className="pagination__Button navigatation previous" 
+                aria-label='page précédente'
+            >
+            </button>
             {pages.map((page) =>
-            <button key={page} className={`pagination__Button ${currentPage === page ? 'active' : ''}`} aria-pressed={`${currentPage === page ? 'true' : 'false'}`}>{page}</button>
+                <button 
+                    key={page}
+                    onClick={() => selectPage(page)} 
+                    className={`pagination__Button ${currentPage === page ? 'active' : ''}`} 
+                    aria-pressed={`${currentPage === page ? 'true' : 'false'}`}
+                >
+                    {page}
+                </button>
             )}
-            <button onClick={nextPage} className="pagination__Button navigatation next" aria-label='page suivante'></button>
+            <button 
+                onClick={nextPage} 
+                className="pagination__Button navigatation next" 
+                aria-label='page suivante'
+            >
+            </button>
         </ul>
     )
 
